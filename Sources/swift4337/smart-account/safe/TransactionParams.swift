2f0c7f4f167bcbba3004a12f9cbe6ca1b979677b
Alexandre THOMAS <athomas.nc@gmail.com>
2024-11-07 15:28:54 +1100

    feat: set struct constructor public

--- Sources/swift4337/smart-account/safe/TransactionParams.swift
+++ Sources/swift4337/smart-account/safe/TransactionParams.swift
@@ -7,5 +7,12 @@ public struct TransactionParams {
     let to: EthereumAddress
     let value: BigUInt
     let data: Data
-    let delegateCall: Bool = false
+    let delegateCall: Bool
+    
+    public init(to: EthereumAddress, value: BigUInt, data: Data, delegateCall: Bool = false) {
+        self.to = to
+        self.value = value
+        self.data = data
+        self.delegateCall = delegateCall
+    }
 }
